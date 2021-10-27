9f0080e9318ed8cd1fc1cf3bede5a14706f2f354
DCinAC <61953469+DCinAC@users.noreply.github.com>
2021-10-28 00:53:06 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,54 @@
+/**
+ * TBV= To Be Validated
+ */
+radio.onReceivedNumber(function (receivedNumber) {
+    mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Run, receivedNumber * PRIV_mult)
+})
+radio.onReceivedString(function (receivedString) {
+    enabled = true
+})
+input.onGesture(Gesture.Shake, function () {
+    shake_count += 1
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "time") {
+        TBV_time = value
+    } else if (name == "multiplier") {
+        TBV_mult = value
+    } else {
+    	
+    }
+    if (TBV_time != 3000 && (TBV_time != 5000 && TBV_time != 8000)) {
+        PRIV_time = 9999
+    } else {
+        PRIV_time = TBV_time
+    }
+    if (TBV_time == 3000 && !(TBV_mult == 15) || (TBV_time == 5000 && !(TBV_mult == 10) || TBV_time == 8000 && !(TBV_mult == 5))) {
+        PRIV_mult = 0
+    } else {
+        PRIV_mult = TBV_mult
+    }
+})
+/**
+ * <- remote
+ * 
+ *            car->
+ */
+let PRIV_time = 0
+let TBV_mult = 0
+let TBV_time = 0
+let shake_count = 0
+let PRIV_mult = 0
+let enabled = false
+led.enable(false)
+radio.setGroup(69)
+enabled = false
+radio.sendValue("time", 3000)
+radio.sendValue("multiplier", 15)
+basic.forever(function () {
+    if (enabled) {
+        basic.pause(3000)
+        radio.sendNumber(shake_count)
+        shake_count = 0
+    }
+})
